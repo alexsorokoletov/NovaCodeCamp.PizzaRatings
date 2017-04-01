f4edaa690060ea92460cedd75f07efff7247c541
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2017-04-01 10:44:22 -0400

    New application

---
+++ Pizza/PizzaPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace Pizza
+{
+	public partial class PizzaPage : ContentPage
+	{
+		public PizzaPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+		{
+			ratingLabel.Text = $"Pizza rating: {ratingStepper.Value}";
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			var pizzaPlace = place.Text;
+			if (string.IsNullOrWhiteSpace(pizzaPlace))
+				return;
+
+			var ratingValue = ratingStepper.Value;
+			var message = $"Your rating {ratingValue} for {pizzaPlace} accepted!";
+			DisplayAlert("Pizza rating", message, "OK");
+		}
+	}
+}
